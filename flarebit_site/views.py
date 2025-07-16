b1a7e6c863ce11e04b8075dc76c54a5adfd4de7f
Rafig Pashayev <pashayevler@gmail.com>
2025-07-16 15:19:41 +0400

    add accounts

--- flarebit_site/views.py
+++ flarebit_site/views.py
@@ -157,6 +157,20 @@ def blog_category(request, slug):
         blogs = paginator.page(1)
     except EmptyPage:
         blogs = paginator.page(paginator.num_pages)
+
+
+    for blog in blogs:
+        if blog.description:
+            blog.cleaned_description = mark_safe(clean_dangerous_html(blog.description))
+        else:
+            blog.cleaned_description = ""
+
+    # Recent blogs için de temizle
+    for blog in recent_blogs:
+        if blog.description:
+            blog.cleaned_description = mark_safe(clean_dangerous_html(blog.description))
+        else:
+            blog.cleaned_description = ""
     
     context = {
         'title': f"{settings.title} - {category.name}",
